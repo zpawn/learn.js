082ed2828dd1267e595947aefba7faff612fb952
zpawn <ilia.makarov@me.com>
2017-12-01 20:47:42 +0200

    webpack: add babel & uglify

--- webpack/src/Feed/index.js
+++ webpack/src/Feed/index.js
@@ -1,3 +1,8 @@
+import _ from 'lodash';
+import moment from 'moment';
+import { markdown } from 'markdown';
+import './index.css';
+
 // 1. Fetch the new data
 fetch('/data')
     .then(res => res.json())
@@ -10,15 +15,14 @@ const addPostForm = document.querySelector('#add-post');
 addPostForm.addEventListener('submit', (event) => {
     event.preventDefault();
     const content = addPostForm.querySelector('.add-post__textarea').value;
-    addNewRecord(window.markdown.toHTML(content));
+    addNewRecord(markdown.toHTML(content));
 });
 
 // 3. Methods
 const renderData = (data) => {
     const target = document.querySelector('#records');
 
-    const html = window._(data.items)
-        .map(item => generateRecord(item.date, window.markdown.toHTML(item.content)))
+    const html = _.map(data.items, item => generateRecord(item.date, markdown.toHTML(item.content)))
         .join('');
 
     target.innerHTML += html;
@@ -32,7 +36,7 @@ const addNewRecord = (content) => {
 
 const generateRecord = (date, content) => {
     return `<section class="record">
-        <div class="record__date">${window.moment(date).format('D MMM YYYY')}</div>
+        <div class="record__date">${moment(date).format('D MMM YYYY')}</div>
         <div class="record__content">${content}</div>
     </section>`;
 }
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,9 +1,39 @@
-const path = require('path');
+const path = require('path'),
+    webpack = require('webpack');
 
 module.exports = {
-    entry: './src/index.js',
+    entry: './src/Feed/index.js',
     output: {
         path: path.resolve(__dirname, 'public', 'feed'),
-        filename: 'index .js'
-    }
+        filename: 'index.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [ 'style-loader', 'css-loader' ]
+            },
+            {
+                test: /\.js$/,
+                exclude: '/node_modules/',
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: [["env", {
+                                targets: {
+                                    browsers: ["last 2 versions", "ie >= 11"]
+                                }
+                            }]],
+                            plugins: ['lodash']
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin(),
+        new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/)
+    ]
 };
