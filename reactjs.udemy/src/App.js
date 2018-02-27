9a169706f9471d0179fad5147c1a54f3ff58ea25
zpawn <ilia.makarov@me.com>
2018-02-27 11:25:52 +0200

    react.udemy: create element without jsx

--- reactjs.udemy/src/App.js
+++ reactjs.udemy/src/App.js
@@ -3,10 +3,8 @@ import './App.css';
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-          <h1>Hello, Create React App</h1>
-      </div>
+    return React.createElement('div', { className: 'App' },
+        React.createElement('h1', null, 'React without JSX' )
     );
   }
 }
