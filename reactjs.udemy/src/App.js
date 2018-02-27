4ac95714f5200d7fe0a64268eb992a36ef9be1d2
zpawn <ilia.makarov@me.com>
2018-02-27 13:41:03 +0200

    Event Handle & Manipulating with state

--- reactjs.udemy/src/App.js
+++ reactjs.udemy/src/App.js
@@ -12,13 +12,23 @@ class App extends Component {
         ]
     };
 
+    switchNameHandler = () => {
+        this.setState({
+            persons: [
+                { name: 'Maximilian', age: 28 },
+                { name: 'Manu', age: 29, hobbies: 'Racing' },
+                { name: 'Stephanie', age: 27 }
+            ]
+        });
+    };
+
     render() {
         return (
             <div className="App">
                 <h1>Hi, I'm a React App</h1>
                 <p>This is really working!</p>
 
-                <button>Switch Name</button>
+                <button onClick={this.switchNameHandler}>Switch Name</button>
 
                 <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
                 <Person name={this.state.persons[1].name} age={this.state.persons[1].age}></Person>
