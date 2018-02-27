4eece5a7509aa27024a7c5766d64eb2110c27660
zpawn <ilia.makarov@me.com>
2018-02-27 13:20:21 +0200

    react.udemy: using state

--- reactjs.udemy/src/App.js
+++ reactjs.udemy/src/App.js
@@ -3,17 +3,29 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
-  render() {
-    return (
-        <div className="App">
-            <h1>Hi, I'm a React App</h1>
-            <p>This is really working!</p>
-            <Person name="Max" age="28" />
-            <Person name="Manu" age="29">My Hobbies: Racing</Person>
-            <Person name="Stephanie" age="26"/>
-        </div>
-    );
-  }
+
+    state = {
+        persons: [
+            { name: 'Max', age: 28 },
+            { name: 'Manu', age: 29, hobbies: 'Racing' },
+            { name: 'Stephanie', age: 26 }
+        ]
+    };
+
+    render() {
+        return (
+            <div className="App">
+                <h1>Hi, I'm a React App</h1>
+                <p>This is really working!</p>
+
+                <button>Switch Name</button>
+
+                <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+                <Person name={this.state.persons[1].name} age={this.state.persons[1].age}></Person>
+                <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+            </div>
+        );
+    }
 }
 
 export default App;
