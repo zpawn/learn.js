d1c2d16d334027ff1f5c40c980ace0fefeaa0416
zpawn <ilia.makarov@me.com>
2018-02-28 13:13:33 +0200

    react.udemy: generate list

--- reactjs.udemy/src/App.js
+++ reactjs.udemy/src/App.js
@@ -13,16 +13,6 @@ class App extends Component {
         showPersons: false
     }
 
-    switchNameHandler = (newName) => {
-        this.setState({
-            persons: [
-                { name: newName, age: 28 },
-                { name: 'Manu', age: 29 },
-                { name: 'Stephanie', age: 27 }
-            ]
-        });
-    }
-
     nameChangedHandler = (event) => {
         this.setState({
             persons: [
@@ -47,6 +37,20 @@ class App extends Component {
             cursor: 'pointer'
         };
 
+        let persons = null;
+
+        if (this.state.showPersons) {
+            persons = (
+                <div>
+                    {
+                        this.state.persons.map(person => {
+                            return <Person name={person.name} age={person.age} />
+                        })
+                    }
+                </div>
+            );
+        }
+
         return (
             <div className="App">
                 <h1>Hi, I'm a React App</h1>
@@ -56,22 +60,7 @@ class App extends Component {
                     style={style}
                     onClick={this.togglePersonsHandler}>Toggle Persons</button>
 
-                {!this.state.showPersons
-                    ? null
-                    : <div>
-                        <Person
-                            name={this.state.persons[0].name}
-                            age={this.state.persons[0].age} />
-                        <Person
-                            name={this.state.persons[1].name}
-                            age={this.state.persons[1].age}
-                            click={this.switchNameHandler.bind(this, 'Maximus')}
-                            changed={this.nameChangedHandler}>My Hobbies: 'Racing'</Person>
-                        <Person
-                            name={this.state.persons[2].name}
-                            age={this.state.persons[2].age}/>
-                    </div>
-                }
+                {persons}
             </div>
         );
     }
