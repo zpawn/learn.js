dffee0676ace0e8946a39b2e2fc3057fce8beaca
zpawn <ilia.makarov@me.com>
2018-03-01 12:06:54 +0200

    react.udemy: dynamically add classes

--- reactjs.udemy/src/App.js
+++ reactjs.udemy/src/App.js
@@ -74,10 +74,12 @@ class App extends Component {
             style.backgroundColor = 'red';
         }
 
+        let classes = ['red', 'bold'].join(' ');
+
         return (
             <div className="App">
                 <h1>Hi, I'm a React App</h1>
-                <p>This is really working!</p>
+                <p className={classes}>This is really working!</p>
 
                 <button
                     style={style}
