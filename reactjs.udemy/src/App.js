a7bb3a4d64b7e5ba3725362ca61ff074b17cc5ea
zpawn <ilia.makarov@me.com>
2018-03-01 16:14:30 +0200

    react.udemy: Enabling in webpack config & Using CSS Modules

--- reactjs.udemy/src/App.js
+++ reactjs.udemy/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import nanoid from 'nanoid';
-import './App.css';
+
+import classes from './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
@@ -74,20 +75,20 @@ class App extends Component {
             style.backgroundColor = 'red';
         }
 
-        let classes = [];
+        let assignedClasses = [];
 
         if (this.state.persons.length <= 2) {
-            classes.push('red');
+            assignedClasses.push(classes.red);
         }
 
         if (this.state.persons.length <= 1) {
-            classes.push('bold');
+            assignedClasses.push(classes.bold);
         }
 
         return (
-            <div className="App">
+            <div className={classes.App}>
                 <h1>Hi, I'm a React App</h1>
-                <p className={classes.join(' ')}>This is really working!</p>
+                <p className={assignedClasses.join(' ')}>This is really working!</p>
 
                 <button
                     style={style}
