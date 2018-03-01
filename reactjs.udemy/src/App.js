40d6f80ce256083053bdc61d6c7ba9d3739c0769
zpawn <ilia.makarov@me.com>
2018-03-01 18:18:08 +0200

    react.udemy: Debugging Using Error Boundaries (React 16+)

--- reactjs.udemy/src/App.js
+++ reactjs.udemy/src/App.js
@@ -3,6 +3,7 @@ import nanoid from 'nanoid';
 
 import classes from './App.css';
 import Person from './Person/Person';
+import ErrorBoundary from './ErrorBoundary/ErrorBoundary';
 
 class App extends Component {
 
@@ -50,13 +51,14 @@ class App extends Component {
                 <div>
                     {
                         this.state.persons.map(person => {
-                            return <Person
-                                key={person.id}
-                                name={person.name}
-                                age={person.age}
-                                click={this.deletePersonHandler.bind(this, person.id)}
-                                changed={event => this.nameChangedHandler(event, person.id)}
-                            />
+                            return <ErrorBoundary key={person.id}>
+                                <Person
+                                    name={person.name}
+                                    age={person.age}
+                                    click={this.deletePersonHandler.bind(this, person.id)}
+                                    changed={event => this.nameChangedHandler(event, person.id)}
+                                />
+                            </ErrorBoundary>
                         })
                     }
                 </div>
--- reactjs.udemy/src/Person/Person.js
+++ reactjs.udemy/src/Person/Person.js
@@ -3,6 +3,13 @@ import React from 'react';
 import classes from './Person.css';
 
 const person = props => {
+
+    const rnd = Math.random();
+
+    if (rnd > 0.7) {
+        throw new Error('Something wont wrong');
+    }
+
     return (
         <div className={classes.Person}>
             <p onClick={props.click}>I'm a {props.name} and I am {props.age} years old!</p>
