5e14edfd0b850c95c00bbbac586c695ee1097fbe
zpawn <ilia.makarov@me.com>
2018-03-02 13:56:26 +0200

    reacr.udemy: Converting Stateless to Stateful Component

--- reactjs.udemy/src/components/Persons/Persons.js
+++ reactjs.udemy/src/components/Persons/Persons.js
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Person from './Person/Person';
 
-const persons = (props) => props.persons.map(person => {
-    return <Person
-        key={person.id}
-        name={person.name}
-        age={person.age}
-        click={() => props.clicked(person.id)}
-        changed={(event) => props.changed(event, person.id)}
-    />
-});
+class Persons extends Component {
+    render() {
+        return this.props.persons.map(person => {
+            return <Person
+                key={person.id}
+                name={person.name}
+                age={person.age}
+                click={() => this.props.clicked(person.id)}
+                changed={(event) => this.props.changed(event, person.id)}
+            />
+        });
+    }
+}
 
-export default persons;
+export default Persons;
