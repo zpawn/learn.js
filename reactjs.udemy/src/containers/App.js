f2610716442b794104b6b0166cfe3621ecc79367
zpawn <ilia.makarov@me.com>
2018-03-02 12:12:33 +0200

    react.udemy: Comparing Stateless & Stateful Components

--- reactjs.udemy/src/containers/App.js
+++ reactjs.udemy/src/containers/App.js
@@ -54,6 +54,7 @@ class App extends Component {
         return (
             <div className={classes.App}>
                 <Cockpit
+                    title={this.props.appTitle}
                     showPersons={this.state.showPersons}
                     persons={this.state.persons}
                     clicked={this.togglePersonsHandler}
