e5e83d9e395ad986bdd6ca9f1897417fd7e47e40
zpawn <ilia.makarov@me.com>
2018-03-12 17:25:59 +0200

    react.burger: Removing Ingredients Safly

--- reactjs.udemy/burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ reactjs.udemy/burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -41,16 +41,41 @@ class BurgerBuilder extends Component {
     }
 
     removeIngredientHandler = (type) => {
-        const oldCount = this.state.ingredients[type],
-            updatedCount = (oldCount - 1 < 0) ? 0 : oldCount - 1;
+        const oldCount = this.state.ingredients[type];
+
+        if (oldCount <= 0) {
+            return;
+        }
+
+        const updatedCount = oldCount - 1,
+            updatedIngredients = { ...this.state.ingredients };
+
+        updatedIngredients[type] = updatedCount;
+
+        const priceDeduction = INGREDIENT_PRICES[type],
+            oldPrice = this.state.totalPrice,
+            newPrice = oldPrice - priceDeduction;
+
+        this.setState({
+            ingredients: updatedIngredients,
+            totalPrice: newPrice
+        });
     }
 
     render () {
+        const disabledInfo = { ...this.state.ingredients };
+        for (let key in disabledInfo) {
+            disabledInfo[key] = disabledInfo[key] <= 0;
+        }
+
         return (
             <Aux>
                 <Burger ingredients={this.state.ingredients} />
                 <BuildControls
-                    ingredientAdded={this.addIngredientHandler}/>
+                    ingredientAdded={this.addIngredientHandler}
+                    ingredientRemoved={this.removeIngredientHandler}
+                    disabled={disabledInfo}
+                />
             </Aux>
         );
     }
