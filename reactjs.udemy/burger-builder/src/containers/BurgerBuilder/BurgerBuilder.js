80b3098f219db36c2170f3daaf2edb6b463d874d
zpawn <ilia.makarov@me.com>
2018-03-12 18:07:27 +0200

    react.burger: Adding the Order Button

--- reactjs.udemy/burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ reactjs.udemy/burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -20,7 +20,16 @@ class BurgerBuilder extends Component {
             cheese: 0,
             meat: 0
         },
-        totalPrice: 4
+        totalPrice: 4,
+        purchasable: false
+    }
+
+    updatePurchaseState = (ingredients) => {
+        const sum = Object.keys(ingredients)
+                .map(igKey => ingredients[igKey] )
+                .reduce((sum, el) => sum + el, 0);
+
+        this.setState({ purchasable: sum > 0 })
     }
 
     addIngredientHandler = (type) => {
@@ -38,6 +47,7 @@ class BurgerBuilder extends Component {
             ingredients: updatedIngredients,
             totalPrice: newPrice
         });
+        this.updatePurchaseState(updatedIngredients);
     }
 
     removeIngredientHandler = (type) => {
@@ -60,6 +70,8 @@ class BurgerBuilder extends Component {
             ingredients: updatedIngredients,
             totalPrice: newPrice
         });
+
+        this.updatePurchaseState(updatedIngredients);
     }
 
     render () {
@@ -75,6 +87,7 @@ class BurgerBuilder extends Component {
                     ingredientAdded={this.addIngredientHandler}
                     ingredientRemoved={this.removeIngredientHandler}
                     disabled={disabledInfo}
+                    purchasable={this.state.purchasable}
                     price={this.state.totalPrice}
                 />
             </Aux>
