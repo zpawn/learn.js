42957f0f77ee67a56cb7fe5df99aa3e26f57d06b
zpawn <ilia.makarov@me.com>
2018-03-14 15:10:21 +0200

    react.burger: Sending a POST Request

--- reactjs.udemy/burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ reactjs.udemy/burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,6 +5,7 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from '../../axios-orders';
 
 const INGREDIENT_PRICES = {
     salad: 0.5,
@@ -86,7 +87,23 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        alert('Continue!!!');
+        const order = {
+            ingredients: this.state.ingredients,
+            price: this.state.totalPrice,
+            customer: {
+                name: 'Max Schwarzmuller',
+                address: {
+                    street: 'testStreet, 1',
+                    zipCode: '41351',
+                    country: 'Germany'
+                },
+                email: 'test@test.com',
+            },
+            deliveryMethod: 'fastest'
+        };
+        axios.post('/orders.json', order)
+            .then(res => console.log('>>> res:', res))
+            .catch(err => console.log('>>> err:', err));
     }
 
     render () {
