b155b51130fc24b0478195cc45bb468e1275c954
zpawn <ilia.makarov@me.com>
2018-03-14 12:26:41 +0200

    react.http: Making a Post Selectable

--- reactjs.udemy/http/src/containers/Blog/Blog.js
+++ reactjs.udemy/http/src/containers/Blog/Blog.js
@@ -9,11 +9,12 @@ import './Blog.css';
 class Blog extends Component {
 
     state = {
-        posts: []
+        posts: [],
+        selectedPostId: null
     }
 
     componentDidMount () {
-        const posts = axios.get('https://jsonplaceholder.typicode.com/posts')
+        axios.get('https://jsonplaceholder.typicode.com/posts')
             .then(res => {
                 const posts = res.data.slice(0, 4),
                     updatedPosts = posts.map(post => {
@@ -23,14 +24,22 @@ class Blog extends Component {
                         }
                     });
                 this.setState({ posts: updatedPosts });
-                console.log('>>>', updatedPosts);
             });
+    }
+
+    postSelectedHandler = (id) => {
+        this.setState({ selectedPostId: id });
     }
 
     render () {
 
         const posts = this.state.posts.map(post => {
-            return <Post key={post.id} title={post.title} author={post.author}/>
+            return <Post
+                key={post.id}
+                title={post.title}
+                author={post.author}
+                clicked={() => this.postSelectedHandler(post.id)}
+            />
         });
 
         return (
@@ -39,7 +48,7 @@ class Blog extends Component {
                     {posts}
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost id={this.state.selectedPostId} />
                 </section>
                 <section>
                     <NewPost />
