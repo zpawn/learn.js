6650b7714d9d1362093be2af2e2ca1d129ed66cb
zpawn <ilia.makarov@me.com>
2018-03-15 11:07:54 +0200

    react.blog: Setting Up Router

--- reactjs.udemy/blog/src/containers/Blog/Blog.js
+++ reactjs.udemy/blog/src/containers/Blog/Blog.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
 
 import './Blog.css';
 import Posts from './Posts/Posts';
@@ -15,7 +16,8 @@ class Blog extends Component {
                         </ul>
                     </nav>
                 </header>
-                <Posts/>
+                <Route path="/" render={() => <h1>Home!</h1>}/>
+                <Route path="/" exact render={() => <h1>Home2</h1>}/>
             </div>
         );
     }
