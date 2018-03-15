e0fee69190b02bbf0b56cd7652ba73f0adf2c277
zpawn <ilia.makarov@me.com>
2018-03-15 11:10:28 +0200

    react.blog: Rendering Components for Routes

--- reactjs.udemy/blog/src/containers/Blog/Blog.js
+++ reactjs.udemy/blog/src/containers/Blog/Blog.js
@@ -3,6 +3,7 @@ import { Route } from 'react-router-dom';
 
 import './Blog.css';
 import Posts from './Posts/Posts';
+import NewPost from './NewPost/NewPost';
 
 class Blog extends Component {
     render () {
@@ -16,8 +17,8 @@ class Blog extends Component {
                         </ul>
                     </nav>
                 </header>
-                <Route path="/" render={() => <h1>Home!</h1>}/>
-                <Route path="/" exact render={() => <h1>Home2</h1>}/>
+                <Route path="/" exact component={Posts}/>
+                <Route path="/new-post" component={NewPost}/>
             </div>
         );
     }
