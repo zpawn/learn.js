dea206400b7b558cc309bca0a57205349007af14
zpawn <ilia.makarov@me.com>
2018-03-15 15:08:30 +0200

    react.udemy.router: Loading Routes Lazily

--- reactjs.udemy/blog/src/containers/Blog/Blog.js
+++ reactjs.udemy/blog/src/containers/Blog/Blog.js
@@ -3,12 +3,17 @@ import { Route, NavLink, Switch, Redirect } from 'react-router-dom';
 
 import './Blog.css';
 import Posts from './Posts/Posts';
-import NewPost from './NewPost/NewPost';
+import asyncComponent from '../../hoc/asyncComponent';
+// import NewPost from './NewPost/NewPost';
+
+const AsyncNewPost = asyncComponent(() => {
+    return import('./NewPost/NewPost');
+});
 
 class Blog extends Component {
 
     state = {
-        auth: false
+        auth: true
     }
 
     render () {
@@ -40,7 +45,7 @@ class Blog extends Component {
                 </header>
 
                 <Switch>
-                    { this.state.auth ? <Route path="/new-post" component={NewPost}/> : null }
+                    { this.state.auth ? <Route path="/new-post" component={AsyncNewPost}/> : null }
                     <Route path="/posts" component={Posts}/>
                     <Route render={() => <h1>Not found!</h1>}/>
                     {/*<Redirect from="/" to="/posts"/>*/}
