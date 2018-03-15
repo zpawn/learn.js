622927a1baee4fabe7eb550ce36137f3a0ec5b12
zpawn <ilia.makarov@me.com>
2018-03-15 14:17:52 +0200

    react.udemy.router: Redirecting Request

--- reactjs.udemy/blog/src/containers/Blog/Blog.js
+++ reactjs.udemy/blog/src/containers/Blog/Blog.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, NavLink, Switch } from 'react-router-dom';
+import { Route, NavLink, Switch, Redirect } from 'react-router-dom';
 
 import './Blog.css';
 import Posts from './Posts/Posts';
@@ -37,6 +37,8 @@ class Blog extends Component {
                 <Switch>
                     <Route path="/new-post" component={NewPost}/>
                     <Route path="/posts" component={Posts}/>
+                    {/*<Route path="/" component={Posts}/>*/}
+                    <Redirect from="/" to="/posts"/>
                 </Switch>
             </div>
         );
