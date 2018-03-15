a94f68aafe4241858243e25ca641d904e95e9bbf
zpawn <ilia.makarov@me.com>
2018-03-15 12:42:44 +0200

    react.ubemy.router: Navigating Programmatically

--- reactjs.udemy/blog/src/containers/Blog/Posts/Posts.js
+++ reactjs.udemy/blog/src/containers/Blog/Posts/Posts.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import axios from "../../../axios";
-import { Link } from 'react-router-dom';
 
 import './Posts.css';
 import Post from '../../../components/Post/Post';
@@ -36,7 +35,8 @@ class Posts extends Component {
     }
 
     postSelectedHandler = (id) => {
-        this.setState({ selectedPostId: id });
+        // this.props.history.push({ pathname: '/' + id });
+        this.props.history.push('/' + id);
     }
 
     render () {
@@ -45,13 +45,14 @@ class Posts extends Component {
         if (!this.state.error) {
             posts = this.state.posts.map(post => {
                 return (
-                    <Link key={post.id} to={'/' + post.id}>
+                    // <Link to={'/' + post.id}>
                         <Post
+                            key={post.id}
                             title={post.title}
                             author={post.author}
                             clicked={() => this.postSelectedHandler(post.id)}
                         />
-                    </Link>
+                    // </Link>
                 )
             });
         }
