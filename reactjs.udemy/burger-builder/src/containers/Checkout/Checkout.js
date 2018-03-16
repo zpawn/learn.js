cf73458b40cff8bc32fc0f49ef3c8de8c9d7e78a
zpawn <ilia.makarov@me.com>
2018-03-16 12:24:31 +0200

    react.udemy.burger: Navigation to the Contact Data Component

--- reactjs.udemy/burger-builder/src/containers/Checkout/Checkout.js
+++ reactjs.udemy/burger-builder/src/containers/Checkout/Checkout.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
 
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
+import ContactData from './ContactData/ContactData';
 
 class Checkout extends Component {
 
@@ -40,6 +42,7 @@ class Checkout extends Component {
                     checkoutCancelled={this.checkoutCancelledHandler}
                     checkoutContinued={this.checkoutContinuedHandler}
                 />
+                <Route path={this.props.match.path + '/contact-data'} component={ContactData}/>
             </div>
         );
     }
