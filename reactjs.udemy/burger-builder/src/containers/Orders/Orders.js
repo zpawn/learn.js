1df71ce9fd946f74707e043597746546b76f261f
zpawn <ilia.makarov@me.com>
2018-03-19 12:14:46 +0200

    react.udemy.burger: Fetching Orders from Firebase

--- reactjs.udemy/burger-builder/src/containers/Orders/Orders.js
+++ reactjs.udemy/burger-builder/src/containers/Orders/Orders.js
@@ -1,10 +1,47 @@
 import React, { Component } from 'react';
 
 import Order from '../../components/Order/Order';
+import axios from '../../axios-orders';
+import Spinner from '../../components/UI/Spinner/Spinner';
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 
 class Orders extends Component {
+    state = {
+        orders: [],
+        loading: true
+    }
+
+    componentDidMount () {
+        axios.get('/orders.json')
+            .then(res => {
+                console.log('>>> res:', res.data);
+                const fetchedOrders = [];
+
+                for (let orderId in res.data) {
+                    fetchedOrders.push({
+                        ...res.data[orderId],
+                        id: orderId
+                    });
+                }
+
+                this.setState({
+                    orders: fetchedOrders,
+                    loading: false
+                });
+            })
+            .catch(err => {
+                this.setState({ loading: false });
+            });
+    }
 
     render () {
+
+        let orders = <Spinner/>;
+
+        if (!this.state.loading) {
+
+        }
+
         return (
             <div>
                 <Order/>
@@ -14,4 +51,4 @@ class Orders extends Component {
     }
 }
 
-export default Orders;
+export default withErrorHandler(Orders, axios);
