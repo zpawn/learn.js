3a50201f5f36f447a82664aeb4a1db18af6bc504
zpawn <ilia.makarov@me.com>
2018-03-20 12:38:27 +0200

    react.udemy.burger: Adding a Dropdown (Select) Component

--- reactjs.udemy/burger-builder/src/components/UI/Input/Input.js
+++ reactjs.udemy/burger-builder/src/components/UI/Input/Input.js
@@ -20,6 +20,15 @@ const input = (props) => {
                 value={props.value}
             />;
             break;
+        case ('select'):
+            inputElement = (
+                <select className={classes.Input} value={props.value}>
+                    {props.elementConfig.options.map((option, index) => (
+                        <option key={index} value={option.value}>{option.displayValue}</option>
+                    ))}
+                </select>
+            );
+            break;
         default:
             inputElement = <input
                 className={classes.InputElement}
