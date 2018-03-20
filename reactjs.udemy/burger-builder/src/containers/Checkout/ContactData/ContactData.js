6936c08e00dd35c1794c97c6c4e27443da10ca0c
zpawn <ilia.makarov@me.com>
2018-03-20 16:52:55 +0200

    Improving Visual Feedback

--- reactjs.udemy/burger-builder/src/containers/Checkout/ContactData/ContactData.js
+++ reactjs.udemy/burger-builder/src/containers/Checkout/ContactData/ContactData.js
@@ -19,7 +19,8 @@ class ContactData extends Component {
                 validation: {
                     required: true
                 },
-                valid: false
+                valid: false,
+                touched: false
             },
             street: {
                 elementType: 'input',
@@ -31,7 +32,8 @@ class ContactData extends Component {
                 validation: {
                     required: true
                 },
-                valid: false
+                valid: false,
+                touched: false
             },
             zipCode: {
                 elementType: 'input',
@@ -45,7 +47,8 @@ class ContactData extends Component {
                     minLength: 5,
                     maxLength: 5
                 },
-                valid: false
+                valid: false,
+                touched: false
             },
             country: {
                 elementType: 'input',
@@ -57,7 +60,8 @@ class ContactData extends Component {
                 validation: {
                     required: true
                 },
-                valid: false
+                valid: false,
+                touched: false
             },
             email: {
                 elementType: 'input',
@@ -69,7 +73,8 @@ class ContactData extends Component {
                 validation: {
                     required: true
                 },
-                valid: false
+                valid: false,
+                touched: false
             },
             deliveryMethod: {
                 elementType: 'select',
@@ -112,6 +117,7 @@ class ContactData extends Component {
 
         updatedFormElement.value = e.target.value;
         updatedFormElement.valid = this.checkValidity(updatedFormElement.value, updatedFormElement.validation);
+        updatedFormElement.touched = true;
         updatedOrderForm[fieldName] = updatedFormElement;
 
         console.log('>>> afterValidation:', updatedOrderForm[fieldName].valid);
@@ -159,6 +165,7 @@ class ContactData extends Component {
                         value={this.state.orderForm[fieldName].value}
                         invalid={!this.state.orderForm[fieldName].valid}
                         shouldValidate={this.state.orderForm[fieldName].validation}
+                        touched={this.state.orderForm[fieldName].touched}
                         changed={(e) => this.inputChangedHandler(e, fieldName)}
                     />
                 ))}
