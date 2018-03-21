4fa5384d25000209c290295007b152e58a72d59e
zpawn <ilia.makarov@me.com>
2018-03-21 12:18:17 +0200

    react.udemy.redux: Adding Subscriptions

--- reactjs.udemy/redux/redux-basics.js
+++ reactjs.udemy/redux/redux-basics.js
@@ -7,6 +7,21 @@ const redux = require('redux'),
 
 // Reducer
 const rootReducer = (state = initState, action) => {
+
+    if (action.type === 'INC_COUNTER') {
+        return {
+            ...state,
+            counter: state.counter + 1
+        };
+    }
+
+    if (action.type === 'ADD_COUNTER') {
+        return {
+            ...state,
+            counter: state.counter + action.value
+        };
+    }
+
     return state;
 };
 
@@ -14,6 +29,12 @@ const rootReducer = (state = initState, action) => {
 const store = createStore(rootReducer);
 console.log(store.getState());
 
-// Dispatching Action
-
 // Subscription
+store.subscribe(() => {
+    console.log('[Subscription]', store.getState());
+});
+
+// Dispatching Action
+store.dispatch({ type: 'INC_COUNTER' });
+store.dispatch({ type: 'ADD_COUNTER', value: 10 });
+console.log(store.getState());
