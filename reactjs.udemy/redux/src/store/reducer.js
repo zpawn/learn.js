146e245bd5d3ca221ac9085f6fa58966e864f30e
zpawn <ilia.makarov@me.com>
2018-03-21 16:29:39 +0200

    react.udemy.redux: Outsoursing Action Types

--- reactjs.udemy/redux/src/store/reducer.js
+++ reactjs.udemy/redux/src/store/reducer.js
@@ -1,3 +1,5 @@
+import * as actionTypes from './actions';
+
 const initialState = {
     counter: 0,
     results: []
@@ -5,30 +7,30 @@ const initialState = {
 
 const reducer = (state = initialState, action) => {
     switch (action.type) {
-        case 'INCREMENT':
+        case actionTypes.INCREMENT:
             const newState = Object.assign({}, state);
             newState.counter = state.counter + 1;
             return newState;
 
-        case 'DECREMENT':
+        case actionTypes.DECREMENT:
             return {
                 ...state,
                 counter: state.counter - 1
             };
 
-        case 'ADD':
+        case actionTypes.ADD:
             return {
                 ...state,
                 counter: state.counter + action.val
             };
 
-        case 'SUBTRACT':
+        case actionTypes.SUBTRACT:
             return {
                 ...state,
                 counter: state.counter - action.val
             };
 
-        case 'STORE_RESULT':
+        case actionTypes.STORE_RESULT:
             return {
                 ...state,
                 results: state.results.concat({
@@ -37,11 +39,7 @@ const reducer = (state = initialState, action) => {
                 })
             };
 
-        case 'DELETE_RESULT':
-            // const id = 2,
-            //     newArray = [...state.results];
-            //
-            // newArray.splice(id, 1);
+        case actionTypes.DELETE_RESULT:
             const updatedArray = state.results.filter(result => result.id !== action.resultElId);
 
             return {
