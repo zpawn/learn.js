8a091c692280fef00245a427d02ba7fbea0f80d4
zpawn <ilia.makarov@me.com>
2018-03-21 13:10:18 +0200

    react.udemy.redux: Passing and Retrieving Data with Action

--- reactjs.udemy/redux/src/store/reducer.js
+++ reactjs.udemy/redux/src/store/reducer.js
@@ -13,11 +13,11 @@ const reducer = (state = initialState, action) => {
     }
 
     if (action.type === 'ADD') {
-        return { counter: state.counter + action.value }
+        return { counter: state.counter + action.val }
     }
 
     if (action.type === 'SUBTRACT') {
-        return { counter: state.counter - action.value }
+        return { counter: state.counter - action.val }
     }
 
     return state;
