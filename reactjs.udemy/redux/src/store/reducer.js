ce5b8953db3344799f40dd3a271bb10abdbef928
zpawn <ilia.makarov@me.com>
2018-03-21 13:15:44 +0200

    react.udemy.redux: Switch-Case in the Reducer

--- reactjs.udemy/redux/src/store/reducer.js
+++ reactjs.udemy/redux/src/store/reducer.js
@@ -4,23 +4,18 @@ const initialState = {
 
 const reducer = (state = initialState, action) => {
 
-    if (action.type === 'INCREMENT') {
-        return { counter: state.counter + 1 }
+    switch (action.type) {
+        case 'INCREMENT':
+            return { counter: state.counter + 1 };
+        case 'DECREMENT':
+            return { counter: state.counter - 1 };
+        case 'ADD':
+            return { counter: state.counter + action.val };
+        case 'SUBTRACT':
+            return { counter: state.counter - action.val };
+        default:
+            return state;
     }
-
-    if (action.type === 'DECREMENT') {
-        return { counter: state.counter - 1 }
-    }
-
-    if (action.type === 'ADD') {
-        return { counter: state.counter + action.val }
-    }
-
-    if (action.type === 'SUBTRACT') {
-        return { counter: state.counter - action.val }
-    }
-
-    return state;
 };
 
 export default reducer;
