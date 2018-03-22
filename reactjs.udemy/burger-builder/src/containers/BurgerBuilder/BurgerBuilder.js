a5c9daf7258f6daf57bb9654ebd0506436cc1888
zpawn <ilia.makarov@me.com>
2018-03-22 13:25:49 +0200

    react.udemy.burger: Redux & UI State

--- reactjs.udemy/burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ reactjs.udemy/burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -14,7 +14,6 @@ import * as actionTypes from '../../store/actions';
 class BurgerBuilder extends Component {
 
     state = {
-        purchasable: false,
         purchasing: false,
         loading: false,
         error: false
@@ -35,7 +34,7 @@ class BurgerBuilder extends Component {
                 .map(igKey => ingredients[igKey] )
                 .reduce((sum, el) => sum + el, 0);
 
-        this.setState({ purchasable: sum > 0 })
+        return sum > 0;
     }
 
     purchaseHandler = () => {
@@ -83,7 +82,7 @@ class BurgerBuilder extends Component {
                         ingredientRemoved={this.props.onIngredientRemoved}
                         ordered={this.purchaseHandler}
                         disabled={disabledInfo}
-                        purchasable={this.state.purchasable}
+                        purchasable={this.updatePurchaseState(this.props.ings)}
                         price={this.props.price}
                     />
                 </Aux>
