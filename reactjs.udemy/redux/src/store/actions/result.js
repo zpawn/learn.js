c749ee11b4d71c2462282d271e464261c2a45870
zpawn <ilia.makarov@me.com>
2018-03-22 16:33:44 +0200

    react.udemy.redux: Using Utility Functions

--- reactjs.udemy/redux/src/store/actions/result.js
+++ reactjs.udemy/redux/src/store/actions/result.js
@@ -10,8 +10,6 @@ export const saveResult = (result) => {
 export const storeResult = (result) => {
     return (dispatch, getState) => {
         setTimeout(() => {
-            const oldCounter = getState().ctr.counter;
-            console.log('>>> oldCounter:', oldCounter);
             dispatch(saveResult(result));
         }, 2000);
     }
--- reactjs.udemy/redux/src/store/reducers/result.js
+++ reactjs.udemy/redux/src/store/reducers/result.js
@@ -1,4 +1,5 @@
 import * as actionTypes from '../actions/actionTypes';
+import { updateObject } from '../utility';
 
 const initialState = {
     results: []
@@ -8,21 +9,15 @@ const reducer = (state = initialState, action) => {
     switch (action.type) {
         case actionTypes.STORE_RESULT:
             // Changed Data
-            return {
-                ...state,
-                results: state.results.concat({
-                    id: Date.now(),
-                    value: action.result * 2
-                })
-            };
+            const newResult = state.results.concat({
+                id: Date.now(),
+                value: action.result * 2
+            });
+            return updateObject(state, { results: newResult });
 
         case actionTypes.DELETE_RESULT:
             const updatedArray = state.results.filter(result => result.id !== action.resultElId);
-
-            return {
-                ...state,
-                results: updatedArray
-            };
+            return updateObject(state, { results: updatedArray });
 
         default:
             return state;
