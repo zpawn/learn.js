15d9833ee168c160ab106b5f0ef8cd09bffffd41
zpawn <ilia.makarov@me.com>
2018-03-22 16:41:32 +0200

    react.udemy.redux: a Leaner Switch-Case Statement

--- reactjs.udemy/redux/src/store/reducers/result.js
+++ reactjs.udemy/redux/src/store/reducers/result.js
@@ -5,22 +5,24 @@ const initialState = {
     results: []
 };
 
+const storeResult = (state, action) => {
+    const newResult = state.results.concat({
+        id: Date.now(),
+        value: action.result * 2
+    });
+    return updateObject(state, { results: newResult });
+};
+
+const deleteResult = (state, action) => {
+    const updatedArray = state.results.filter(result => result.id !== action.resultElId);
+    return updateObject(state, { results: updatedArray });
+};
+
 const reducer = (state = initialState, action) => {
     switch (action.type) {
-        case actionTypes.STORE_RESULT:
-            // Changed Data
-            const newResult = state.results.concat({
-                id: Date.now(),
-                value: action.result * 2
-            });
-            return updateObject(state, { results: newResult });
-
-        case actionTypes.DELETE_RESULT:
-            const updatedArray = state.results.filter(result => result.id !== action.resultElId);
-            return updateObject(state, { results: updatedArray });
-
-        default:
-            return state;
+        case actionTypes.STORE_RESULT:  return storeResult(state, action);
+        case actionTypes.DELETE_RESULT: return deleteResult(state, action);
+        default:                        return state;
     }
 };
 
