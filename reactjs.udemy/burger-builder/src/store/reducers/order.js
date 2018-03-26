43e6a70a1ab242cf0e39bd351eea80bcb4f3a974
zpawn <ilia.makarov@me.com>
2018-03-26 15:09:22 +0300

    react.udemy.burger: the Order Reducer

---
+++ reactjs.udemy/burger-builder/src/store/reducers/order.js
@@ -0,0 +1,33 @@
+import * as actionTypes from '../actions/actionTypes';
+
+const initState = {
+    orders: [],
+    leading: false
+};
+
+const reducer = (state = initState, action) => {
+    switch (action.type) {
+        case actionTypes.PURCHASE_BURGER_SUCCESS:
+            const newOrder = {
+                ...action.orderData,
+                id: action.orderId
+            };
+
+            return {
+                ...state,
+                leading: false,
+                orders: state.orders.concat(newOrder)
+            };
+
+        case actionTypes.PURCHASE_BURGER_FAIL:
+            return {
+                ...state,
+                leading: false
+            };
+
+        default:
+            return state;
+    }
+};
+
+export default reducer;
