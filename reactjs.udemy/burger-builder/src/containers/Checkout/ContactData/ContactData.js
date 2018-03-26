82fcf9ca05ea230fb4170c0cf72ab62ae96f7bb5
zpawn <ilia.makarov@me.com>
2018-03-26 15:18:32 +0300

    react.udemy.burger: Working on Order Actions

--- reactjs.udemy/burger-builder/src/containers/Checkout/ContactData/ContactData.js
+++ reactjs.udemy/burger-builder/src/containers/Checkout/ContactData/ContactData.js
@@ -94,8 +94,7 @@ class ContactData extends Component {
                 valid: true
             }
         },
-        formIsValid: false,
-        loading: false
+        formIsValid: false
     }
 
     checkValidity = (value, rules) => {
@@ -187,7 +186,7 @@ class ContactData extends Component {
             </form>
         );
 
-        if (this.state.loading) {
+        if (this.props.loading) {
             form = <Spinner/>
         }
 
@@ -203,13 +202,14 @@ class ContactData extends Component {
 const mapStateToProps = state => {
     return {
         ings: state.ingredients,
-        price: state.totalPrice
+        price: state.totalPrice,
+        loading: state.loading
     };
 };
 
 const mapDispatchToProps = dispatch => {
     return {
-        onOrderBurger: (orderData) => dispatch(actions.purchaseBurgerStart(orderData))
+        onOrderBurger: (orderData) => dispatch(actions.purchaseBurger(orderData))
     };
 };
 
