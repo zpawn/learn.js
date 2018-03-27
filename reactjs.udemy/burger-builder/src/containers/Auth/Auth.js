b650ca923eac469b3cb6e634fdb31a3c491f56fc
zpawn <ilia.makarov@me.com>
2018-03-27 11:47:44 +0300

    react.udemy.burger: Adding a Auth Form

---
+++ reactjs.udemy/burger-builder/src/containers/Auth/Auth.js
@@ -0,0 +1,101 @@
+import React, { Component } from 'react';
+
+import classes from './Auth.css';
+import Input from '../../components/UI/Input/Input';
+import Button from '../../components/UI/Button/Button';
+
+class Auth extends Component {
+
+    state = {
+        controls: {
+            email: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'email',
+                    placeholder: 'E-mail'
+                },
+                value: '',
+                validation: {
+                    required: true,
+                    isEmail: true
+                },
+                valid: false,
+                touched: false
+            },
+            password: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'password',
+                    placeholder: 'Password'
+                },
+                value: '',
+                validation: {
+                    required: true,
+                    minLength: 6
+                },
+                valid: false,
+                touched: false
+            }
+        }
+    }
+
+    checkValidity = (value, rules) => {
+        let isValid = true;
+
+        if (rules.required) {
+            isValid = value.trim() !== '' && isValid;
+        }
+
+        if (rules.minLength) {
+            isValid = value.length >= rules.minLength && isValid;
+        }
+
+        if (rules.isEmail) {
+            const pattern = /.+@.+\..+/g;
+            return pattern.test(value) && isValid;
+        }
+
+        return isValid;
+    }
+
+    inputChangedHandler = (e, controlName) => {
+        const updatedControls = {
+            ...this.state.controls,
+            [controlName]: {
+                ...this.state.controls[controlName],
+                value: e.target.value,
+                valid: this.checkValidity(e.target.value, this.state.controls[controlName].validation),
+                touched: true
+            }
+        };
+
+        this.setState({ controls: updatedControls });
+    }
+
+    render () {
+
+        const formField = Object.keys(this.state.controls);
+
+        return (
+            <div className={classes.Auth}>
+                <form action="">
+                    {formField.map(fieldName => (
+                        <Input
+                            key={fieldName}
+                            elementType={this.state.controls[fieldName].elementType}
+                            elementConfig={this.state.controls[fieldName].elementConfig}
+                            value={this.state.controls[fieldName].value}
+                            invalid={!this.state.controls[fieldName].valid}
+                            shouldValidate={this.state.controls[fieldName].validation}
+                            touched={this.state.controls[fieldName].touched}
+                            changed={(e) => this.inputChangedHandler(e, fieldName)}
+                        />
+                    ))}
+                    <Button btnType="Success">SUBMIT</Button>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default Auth;
