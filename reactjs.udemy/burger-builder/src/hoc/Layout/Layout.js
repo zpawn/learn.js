36e84d8a14856f7f9720ddf171d472ade5c599d4
zpawn <ilia.makarov@me.com>
2018-03-28 14:46:42 +0300

    react.udemy.burger: Updating the UI Depending on Auth State

--- reactjs.udemy/burger-builder/src/hoc/Layout/Layout.js
+++ reactjs.udemy/burger-builder/src/hoc/Layout/Layout.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 import classes from './Layout.css';
 import Aux from '../Aux/Aux';
@@ -24,12 +25,25 @@ class Layout extends Component {
     render () {
         return (
             <Aux>
-                <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler}/>
-                <SideDrawer open={this.state.showSideDrawer} closed={this.sideDrawerClosedHandler} />
+                <Toolbar
+                    isAuth={this.props.isAuthenticated}
+                    drawerToggleClicked={this.sideDrawerToggleHandler}
+                />
+                <SideDrawer
+                    isAuth={this.props.isAuthenticated}
+                    open={this.state.showSideDrawer}
+                    closed={this.sideDrawerClosedHandler}
+                />
                 <main className={classes.Content}>{this.props.children}</main>
             </Aux>
         );
     }
 }
 
-export default Layout;
+const mapStateToProps = state => {
+    return {
+        isAuthenticated: state.auth.token !== null
+    };
+};
+
+export default connect(mapStateToProps)(Layout);
