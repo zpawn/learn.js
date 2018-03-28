3f5db7cd1f39a2431c95385fb785c059bc2b1a5e
zpawn <ilia.makarov@me.com>
2018-03-28 18:55:42 +0300

    react.udemy.burger: Guarding Routes

--- reactjs.udemy/burger-builder/src/App.js
+++ reactjs.udemy/burger-builder/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, Switch, withRouter } from 'react-router-dom';
+import { Route, Switch, withRouter, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import Layout from './hoc/Layout/Layout';
@@ -16,19 +16,40 @@ class App extends Component {
     }
 
     render() {
-        return (
-            <Layout>
+
+        let routes = (
+            <Switch>
+                <Route path="/auth" component={Auth}/>
+                <Route path="/" exact component={BurgerBuilder}/>
+                <Redirect to="/"/>
+            </Switch>
+        );
+
+        if (this.props.isAuth) {
+            routes = (
                 <Switch>
                     <Route path="/checkout" component={Checkout}/>
                     <Route path="/orders" component={Orders}/>
-                    <Route path="/auth" component={Auth}/>
                     <Route path="/logout" component={Logout}/>
                     <Route path="/" exact component={BurgerBuilder}/>
+                    <Redirect to="/"/>
                 </Switch>
+            );
+        }
+
+        return (
+            <Layout>
+                {routes}
             </Layout>
         );
     }
 }
+
+const mapStateToProps = state => {
+    return {
+        isAuth: state.auth.token !== null
+    };
+};
 
 const mapDispatchToProps = dispatch => {
     return {
@@ -36,4 +57,4 @@ const mapDispatchToProps = dispatch => {
     };
 };
 
-export default withRouter(connect(null, mapDispatchToProps)(App));
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App));
