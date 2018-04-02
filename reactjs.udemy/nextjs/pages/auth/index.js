d4cc954a2dbd7c2265bd5b70f825a491614cf40d
zpawn <ilia.makarov@me.com>
2018-04-02 12:13:17 +0300

    react.udemy.nextjs: Styling our App in Next,js

--- reactjs.udemy/nextjs/pages/auth/index.js
+++ reactjs.udemy/nextjs/pages/auth/index.js
@@ -6,6 +6,14 @@ const authIndexPage = () => (
     <div>
         <h1>The Auth Index Page!</h1>
         <User name="Max" age={28}/>
+        <style jsx>{`
+            div {
+                border: 1px solid #eee;
+                box-shadow: 0 2px 3px #ccc;
+                padding: 20px;
+                text-align: center;
+            }
+        `}</style>
     </div>
 );
 
