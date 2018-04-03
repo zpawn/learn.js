1dc71ec844290dfc2b3ea7d6983919ce456d4800
zpawn <ilia.makarov@me.com>
2018-04-03 14:53:18 +0300

    react.udemy.animation: Animation Timing

--- reactjs.udemy/animations/src/components/Modal/Modal.js
+++ reactjs.udemy/animations/src/components/Modal/Modal.js
@@ -3,11 +3,16 @@ import Transition from 'react-transition-group/Transition';
 
 import './Modal.css';
 
+const animationTiming = {
+    enter: 400,
+    exit: 1000
+};
+
 const modal = (props) => {
     return (
         <Transition
             in={props.show}
-            timeout={300}
+            timeout={animationTiming}
             mountOnEnter
             unmountOnExit
         >
