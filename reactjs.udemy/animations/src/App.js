195619c3789743f0cceca75f3a5f452862d9a3a1
zpawn <ilia.makarov@me.com>
2018-04-03 14:47:26 +0300

    react.udemy.animation: Wrapping the Transition Component

--- reactjs.udemy/animations/src/App.js
+++ reactjs.udemy/animations/src/App.js
@@ -48,16 +48,7 @@ class App extends Component {
                     )}
                 </Transition>
 
-                <Transition
-                    in={this.state.modalIsOpen}
-                    timeout={300}
-                    mountOnEnter
-                    unmountOnExit
-                >
-                    {state => (
-                        <Modal show={state} closed={this.closeModal}/>
-                    )}
-                </Transition>
+                <Modal show={this.state.modalIsOpen} closed={this.closeModal}/>
 
                 {this.state.modalIsOpen ? <Backdrop show/> : null}
 
