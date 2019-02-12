298a88cdd64a2977cbc96adf031132048368f3b3
zpawn <ilia.makarov@me.com>
2019-02-12 16:35:57 +0200

    vue.monster-slayer: implementing attack method

--- vue/02-monster-slayer/app.js
+++ vue/02-monster-slayer/app.js
@@ -10,6 +10,41 @@ new Vue({
       this.gameIsRunning = true;
       this.playerHealth = 100;
       this.monsterHealth = 100;
+    },
+
+    attack: function () {
+      this.monsterHealth -= this.calculateDamage(3, 10);
+      if (this.checkWin()) {
+        return;
+      }
+
+      this.playerHealth -= this.calculateDamage(5, 12);
+      this.checkWin();
+    },
+
+    specialAttack: function () {},
+
+    heal: function () {},
+
+    giveUp: function () {},
+
+    calculateDamage: function (min, max) {
+      return Math.max(Math.floor(Math.random() * max) + 1, min)
+    },
+
+    checkWin: function () {
+      if (this.monsterHealth <= 0) {
+        (confirm('You won! New Game?'))
+          ? this.startGame()
+          : this.gameIsRunning = false;
+        return true;
+      } else if (this.playerHealth <= 0) {
+        (confirm('You lost! New Game?'))
+          ? this.startGame()
+          : this.gameIsRunning = false;
+      }
+
+      return false;
     }
   }
 });
