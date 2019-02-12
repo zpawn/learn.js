3b26f0e8a8baadc43d0c598a3fc81fd872e695e6
zpawn <ilia.makarov@me.com>
2019-02-12 14:41:11 +0200

    vue: refactoring

--- vue/01-instance/app.js
+++ vue/01-instance/app.js
@@ -1,28 +1,28 @@
 const vm1 = new Vue({
-    el: '#app1',
-    data: {
-        title: 'The VueJS Instance',
-        showParagraph: false
+  el: '#app1',
+  data: {
+    title: 'The VueJS Instance',
+    showParagraph: false
+  },
+  methods: {
+    show: function () {
+      this.showParagraph = true;
+      this.updateTitle('The VueJS Instance (Updated)');
     },
-    methods: {
-        show: function () {
-            this.showParagraph = true;
-            this.updateTitle('The VueJS Instance (Updated)');
-        },
-        updateTitle: function (title) {
-            this.title = title;
-        }
-    },
-    computed: {
-        lowercaseTitle: function () {
-            return this.title.toLowerCase();
-        }
-    },
-    watch: {
-        title: function (value) {
-            alert('Title changed, new value: ' + value);
-        }
+    updateTitle: function (title) {
+      this.title = title;
     }
+  },
+  computed: {
+    lowercaseTitle: function () {
+      return this.title.toLowerCase();
+    }
+  },
+  watch: {
+    title: function (value) {
+      alert('Title changed, new value: ' + value);
+    }
+  }
 });
 
 setTimeout(() => {
