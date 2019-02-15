832ecf2f974c37116ba99309d4e1562d32b65437
zpawn <ilia.makarov@me.com>
2019-02-15 17:09:52 +0200

    vue: init project via vue-cli

---
+++ vue/03-workflow/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <h1>Hello, World!</h1>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+
+</style>
