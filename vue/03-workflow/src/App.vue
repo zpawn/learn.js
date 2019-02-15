941678ff8c46822e04de4e905d4cdbb6abafa9a7
zpawn <ilia.makarov@me.com>
2019-02-15 18:24:42 +0200

    vue: split into others components

--- vue/03-workflow/src/App.vue
+++ vue/03-workflow/src/App.vue
@@ -1,9 +1,31 @@
 <template>
-  <app-servers></app-servers>
+  <div class="container">
+    <app-header></app-header>
+    <hr>
+    <div class="row">
+      <app-servers></app-servers>
+      <app-server-details></app-server-details>
+    </div>
+    <hr>
+    <app-footer></app-footer>
+  </div>
 </template>
 
 <script>
+  import Header from './components/Header.vue';
+  import Footer from './components/Footer.vue';
+  import Servers from './components/Servers.vue';
+  import ServerDetails from './components/ServerDetails.vue';
 
+  export default {
+    name: "App",
+    components: {
+      'app-header': Header,
+      'app-footer': Footer,
+      'app-servers': Servers,
+      'app-server-details': ServerDetails,
+    },
+  }
 </script>
 
 <style>
---
+++ vue/03-workflow/src/components/ServerDetails.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="col-xs-12 col-sm-6">
+    <p>Server Details are currently not updated</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "ServerDetails",
+  }
+</script>
+
+<style scoped>
+
+</style>
