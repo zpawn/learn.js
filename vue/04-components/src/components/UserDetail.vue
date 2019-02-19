e0392cda6db8efa31c9eb5f8bd16a5b7a004a3ec
zpawn <ilia.makarov@me.com>
2019-02-19 14:09:12 +0200

    vue: passing props from Parent to Child

--- vue/04-components/src/components/UserDetail.vue
+++ vue/04-components/src/components/UserDetail.vue
@@ -2,10 +2,14 @@
   <section class="component">
     <h3>You may view the User Details here</h3>
     <p>Many Details</p>
+    <p>{{ name }}</p>
   </section>
 </template>
 
 <script>
+  export default {
+    props: ['name']
+  }
 </script>
 
 <style scoped>
