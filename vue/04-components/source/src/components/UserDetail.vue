b2fd82becf0ce551e67ccd4d4b5dd56f39984eda
zpawn <ilia.makarov@me.com>
2019-02-19 14:06:49 +0200

    vue: init new дуфктштп ыусешщт

---
+++ vue/04-components/source/src/components/UserDetail.vue
@@ -0,0 +1,15 @@
+<template>
+    <section class="component">
+        <h3>You may view the User Details here</h3>
+        <p>Many Details</p>
+    </section>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+    section {
+        background-color: lightcoral;
+    }
+</style>
---
+++ vue/04-components/source/src/components/UserEdit.vue
@@ -0,0 +1,15 @@
+<template>
+    <section class="component">
+        <h3>You may edit the User here</h3>
+        <p>Edit me!</p>
+    </section>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+    section {
+        background-color: lightgreen;
+    }
+</style>
