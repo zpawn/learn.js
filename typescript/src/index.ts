0801c234780647ed22466200dbb10fa59d2b3896
ilia <i.makarov@andersenlab.com>
2019-08-11 10:59:45 +0300

    primitive types

--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -1,8 +1,40 @@
-export class C {
-    private x = 10
-    getX = () => this.x;
-    setX = (newVal: number) => { this.x = newVal; }
+// string
+let myName: string = 'Max';
+// myName = 28
+
+// number
+let myAge: number = 27
+// myAge = 'String'
+
+// boolean
+let hasHobbies: boolean = false
+// hasHobbies = 1
+
+// assign types
+let myRealAge: number;
+myRealAge = 27;
+// myRealAge = '27';
+
+// array
+let hobbies: any[] = ['Cooking', 'Sports'];
+hobbies[0] = [100];
+// hobbies = 100;
+
+// tuples
+let adress: [string, number] = ['SuperStreet', 99];
+
+// enum
+enum Color {
+  Gray ,
+  Green = 100,
+  Blue,
 }
 
-export let x = new C();
-export let y = { ...{ some: "value" } }
+let myColor: Color = Color.Green;
+console.log(myColor);
+
+// any
+let car: any = "BMW";
+console.log(car);
+car = { brand: 'BMW', series: 3 };
+console.log(car);
